d9185284dd85d9390e9b7d7625641cd787358703
Mariana Bandeira <marianabandeira.cicm@gmail.com>
2025-07-19 20:03:44 -0300

    primeiro commit

---
+++ js-curso-2-desafio_1/app.js
@@ -0,0 +1,10 @@
+// Alterar h1 e atribuir texto 
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio';
+
+// Criar função que exiba no console uma mensagem
+let botaoConsole = document.querySelector('p');
+// Função para verificar se o botão foi clicado
+function verificarClicadoDoBotão() {
+    console.log('Eu amo JS');
+}   
