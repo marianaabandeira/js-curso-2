273f1dfb5f60a8debd820688a967055cffda52aa
Mariana Bandeira <marianabandeira.cicm@gmail.com>
2025-07-20 05:47:57 -0300

    atualizando projeto inicial

--- projeto-inicial/app.js
+++ projeto-inicial/app.js
@@ -1,10 +1,40 @@
-let titulo = document .querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
- 
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+
 function verificarChute(){
-    console.log('o botão foi clicado!');
+    console.log(numeroSecreto);
 
 }
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//Outros: Como aprendi sem ser na maneira reduzida:
+//let titulo = document .querySelector('h1');
+//titulo.innerHTML = 'Jogo do número secreto';
+
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
