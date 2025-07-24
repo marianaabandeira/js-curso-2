ca01394d6a7689b02cbf1b40bcba406a8319cf24
Mariana Bandeira <marianabandeira.cicm@gmail.com>
2025-07-23 22:15:55 -0300

    Desenvolvi mais o projeto inicial e fiz um novo desafio

---
+++ desafio_2/app.js
@@ -0,0 +1,35 @@
+//Criar função que exibe "olá, mundo!" no console
+function exibirMensagem() {
+    console.log('Olá, mundo!');
+}
+
+// Criar função que recebe um nome como parâmetro e exibe uma mensagem personalizada
+function exibirNome(nome) {
+    console.log(`Olá, ${nome}!`);
+
+}
+
+// Criar função que recebe um número e retorna o dobro desse número
+function calcularDobro(numero) {
+    return numero * 2;
+
+}
+
+// Criar função que recebe tres números e retorna a média deles
+function calcularMedia(num1, num2, num3) {
+    return (num1 + num2 + num3) / 3;
+
+}
+
+//Criar função que recebe dois números e retorna o maior deles
+function maiorNumero(num1,num2) {
+   return num1 > num2 ? num1 : num2;
+   
+}
+
+// Criar função que recebe um números e retorna a multiplicação desse número por ele mesmo
+function multiplicarPorEleMesmo(numero) {
+    return numero * numero;
+
+}
+
--- projeto-inicial/app.js
+++ projeto-inicial/app.js
@@ -11,7 +11,8 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 
 function verificarChute(){
-    console.log(numeroSecreto);
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
 
 }
 
